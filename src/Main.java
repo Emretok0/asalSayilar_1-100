8112a29bc5a1c3deb2870dd67d81339fd2ef4401
Emre Tok <emretok4400@gmail.com>
2021-05-23 15:36:24 +0300

    First

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int baslangic = 1, bitis = 100;
+
+        boolean h = true;
+
+        while (baslangic < bitis) {
+            for (int i = 2; i <= baslangic; i++) {
+                if (i != baslangic) {
+                    if (baslangic % i == 0) {
+                        h = false;
+                        break;
+                    }
+                } else {
+                    if (baslangic % i==0){
+                        h = true;
+                    }
+                }
+            }
+            if (h){
+                System.out.println(baslangic);
+            }
+            baslangic++;
+        }
+    }
+}
